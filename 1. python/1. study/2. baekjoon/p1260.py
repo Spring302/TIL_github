234757812d493b8f57f29228e5b9c3da252e5745
jaeyeol <gkwoduf09@gmail.com>
2021-08-22 13:53:27 +0900

    알고리즘 및 RPA 실습

---
+++ 1. python/1. study/2. baekjoon/p1260.py
@@ -0,0 +1,15 @@
+# n,m,v = map(int, input().split())
+
+n,m,v = 4,5,1
+
+graph = dict()
+graph.update(a,b)
+
+
+# graph_list = {1: set([3, 4]),
+#               2: set([3, 4, 5]),
+#               3: set([1, 5]),
+#               4: set([1]),
+#               5: set([2, 6]),
+#               6: set([3, 5])}
+# root_node = 1
